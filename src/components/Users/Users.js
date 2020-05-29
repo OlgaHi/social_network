494bc565d493a5145477618e2d960b3cdf95d9a9
OlgaHi <46238682+OlgaHi@users.noreply.github.com>
2020-05-28 22:13:25 -0700

    updates Users

--- src/components/Users/Users.js
+++ src/components/Users/Users.js
@@ -1,7 +1,19 @@
 import React from 'react';
-import s from './users.module.css';
+import s from './Users.module.css';
+
 
 let Users = (props) => {
+    
+    if (props.users.length === 0) {
+        props.setUsers([
+            {id: 1, photoUrl:'https://img.icons8.com/bubbles/2x/girl-with-compass.png', followed: false, fullName: 'Amanda', status: 'I am happy', location: {city: 'Alicante', country: 'Spain'}}, 
+            {id: 2, photoUrl:'https://img.icons8.com/bubbles/2x/user.png', followed: true, fullName: 'Jack', status: 'Looking for a job', location: {city: 'Seattle', country: 'USA'}}, 
+            {id: 3, photoUrl:'https://comandoestrategico.com/wp-content/uploads/2019/04/administrator-male.png', followed: false, fullName: 'Adam', status: 'React React', location: {city: 'Milan', country: 'Italy'}}
+        ])
+    }
+
+    
+
     return (
         <div>
             {
@@ -11,12 +23,15 @@ let Users = (props) => {
                             <img alt='' src={u.photoUrl} className={s.userPhoto} />
                         </div>
                         <div>
-                            <button>Follow</button>
+                            { u.followed 
+                                ? <button onClick={ () => { props.unfollow(u.id)}}>Unfollow</button>
+                                : <button onClick={ () => { props.follow(u.id)}}>Follow</button>
+                            }
                         </div>
                     </span>
                     <span>
                         <span>
-                            <div>{u.fullname}</div>
+                            <div>{u.fullName}</div>
                             <div>{u.status}</div>
                         </span>
                         <span>
--- src/redux/users-reducer.js
+++ src/redux/users-reducer.js
@@ -3,11 +3,7 @@ const UNFOLLOW = 'UNFOLLOW';
 const SET_USERS = 'SET_USERS';
 
 let initialState = {
-  users: [
-    {id: 1, photoUrl:'https://img.icons8.com/bubbles/2x/girl-with-compass.png', followed: false, fullName: 'Amanda', status: 'I am happy', location: {city: 'Alicante', country: 'Spain'}}, 
-    {id: 2, photoUrl:'https://img.icons8.com/bubbles/2x/user.png', followed: true, fullName: 'Jack', status: 'Looking for a job', location: {city: 'Seattle', country: 'USA'}}, 
-    {id: 3, photoUrl:'https://comandoestrategico.com/wp-content/uploads/2019/04/administrator-male.png', followed: false, fullName: 'Adam', status: 'React React', location: {city: 'Milan', country: 'Italy'}}
-]
+  users: []
 };
 
 const usersReducer = (state = initialState, action) => {
