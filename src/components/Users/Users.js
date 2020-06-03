751836ed10a3ed8725a229b34c5df1ae6988d272
OlgaHi <46238682+OlgaHi@users.noreply.github.com>
2020-06-03 14:54:31 -0700

    added axios get

--- src/components/Users/Users.js
+++ src/components/Users/Users.js
@@ -1,18 +1,18 @@
 import React from 'react';
 import s from './Users.module.css';
-
+import * as axios from 'axios';
+import userPhoto from '../../assets/images/girl-with-compass.png'
 
 let Users = (props) => {
     
     if (props.users.length === 0) {
-        props.setUsers([
-            {id: 1, photoUrl:'https://img.icons8.com/bubbles/2x/girl-with-compass.png', followed: false, fullName: 'Amanda', status: 'I am happy', location: {city: 'Alicante', country: 'Spain'}}, 
-            {id: 2, photoUrl:'https://img.icons8.com/bubbles/2x/user.png', followed: true, fullName: 'Jack', status: 'Looking for a job', location: {city: 'Seattle', country: 'USA'}}, 
-            {id: 3, photoUrl:'https://comandoestrategico.com/wp-content/uploads/2019/04/administrator-male.png', followed: false, fullName: 'Adam', status: 'React React', location: {city: 'Milan', country: 'Italy'}}
-        ])
-    }
 
-    
+        axios.get("https://social-network.samuraijs.com/api/1.0/users")
+             .then(response => {
+                props.setUsers(response.data.items);
+             });
+
+    }
 
     return (
         <div>
@@ -20,7 +20,7 @@ let Users = (props) => {
                 props.users.map(u => <div key={u.id}>
                     <span>
                         <div>
-                            <img alt='' src={u.photoUrl} className={s.userPhoto} />
+                            <img alt='' src={ u.photos.small != null ? u.photos.small : userPhoto} className={s.userPhoto} />
                         </div>
                         <div>
                             { u.followed 
@@ -31,12 +31,12 @@ let Users = (props) => {
                     </span>
                     <span>
                         <span>
-                            <div>{u.fullName}</div>
+                            <div>{u.name}</div>
                             <div>{u.status}</div>
                         </span>
                         <span>
-                            <div>{u.location.country}</div>
-                            <div>{u.location.city}</div>
+                            <div>{"u.location.country"}</div>
+                            <div>{"u.location.city"}</div>
                         </span>
                     </span>
                 </div>)
@@ -46,3 +46,10 @@ let Users = (props) => {
 }
 
 export default Users;
+
+
+/* [
+    {id: 1, photoUrl:'https://img.icons8.com/bubbles/2x/girl-with-compass.png', followed: false, fullName: 'Amanda', status: 'I am happy', location: {city: 'Alicante', country: 'Spain'}}, 
+    {id: 2, photoUrl:'https://img.icons8.com/bubbles/2x/user.png', followed: true, fullName: 'Jack', status: 'Looking for a job', location: {city: 'Seattle', country: 'USA'}}, 
+    {id: 3, photoUrl:'https://comandoestrategico.com/wp-content/uploads/2019/04/administrator-male.png', followed: false, fullName: 'Adam', status: 'React React', location: {city: 'Milan', country: 'Italy'}}
+] */
